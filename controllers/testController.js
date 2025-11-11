60ff83497cbed5d355422c430a79014bf8188827
Aman Singh <amankumarkvm4@gmail.com>
2025-11-11 12:23:02 +0530

    Food-application backend

---
+++ controllers/testController.js
@@ -0,0 +1,13 @@
+const testUserController = (req, resp) => {
+    try {
+        resp.status(200).send({
+            success: true,
+            message: 'test User Data API'
+        })
+    } catch (error) {
+        console.log('error in Test API', error);
+
+    }
+}
+
+module.exports = { testUserController }
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const colors = require('colors')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+
+
+//dot en cofigration
+dotenv.config()
+
+//DB connection
+connectDB()
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/test', require('./routes/testRoutes'))
+
+app.get('/', (req, resp) => {
+    return resp.status(200).send("<h1> Welcome to food server </h1>")
+})
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`.bgMagenta);
+
+})
